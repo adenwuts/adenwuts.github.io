647c6bd6e1d91088b4dcdcbc3439c43c83f826f1
Aden Northcote <13960570@student.uts.edu.au>
2023-11-26 15:35:13 +1100

    add jinja bones

---
+++ test.py
@@ -0,0 +1,30 @@
+from jinja2 import Environment, FileSystemLoader
+from pathlib import Path
+
+import requests
+
+import os
+
+GITHUB_TOKEN = os.environ["PAGES_COMMIT_TOKEN"]
+TEMPLATES_DIR = Path(__file__).parent / "templates"
+
+headers = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": f"Bearer {GITHUB_TOKEN}",
+    "X-GitHub-Api-Version": "2022-11-28",
+}
+
+response = requests.get(
+    "https://api.github.com/repos/adenwuts/adenwuts.github.io/commits?sha=pages",
+    headers=headers,
+)
+
+recent_commits = [
+    (commit["commit"]["message"], commit["commit"]["author"]["date"])
+    for commit in response.json()[:10]
+]
+
+environment = Environment(loader=FileSystemLoader(TEMPLATES_DIR))
+template = environment.get_template("index.html")
+output = template.render(recent_commits=recent_commits)
+print(output)
